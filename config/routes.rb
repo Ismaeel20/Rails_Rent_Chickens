37c30e6dfc552c48d4758e94543467dfa4b3101c
Edward <130746635+Plodder123@users.noreply.github.com>
2023-05-29 17:18:25 +0100

    added chickens routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :chickens, only: [:new, :create, :index, :show]
 end
+# Defines the root path route ("/")
+# root "articles#index"
