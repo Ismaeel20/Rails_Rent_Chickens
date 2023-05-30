c2e96b167df85f2a3783969687822f7f247e0883
ismaeel haque ismaeelhaque10@gmail.com <ismaeelhaque10@gmail.com>
2023-05-30 16:13:30 +0100

    Added index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :chickens, only: [:new, :create, :index, :show]
+  resources :chickens # only: [:new, :create, :index, :show]
+  get "bookings", to: "bookings#index"
+  get "bookings/:id", to: "bookings#show"
+  get "bookings/new", to: "bookings#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
