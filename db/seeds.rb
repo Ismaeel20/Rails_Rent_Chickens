4900637f3aa8bb3e86ab8818426ac148b40c907d
Edward <130746635+Plodder123@users.noreply.github.com>
2023-05-30 16:46:29 +0100

    updated database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "cleaing database"
+# destroy database
+
+puts "creating users"
+edward = User.create(email: "chicken@lewagon.com", password: "123456")
