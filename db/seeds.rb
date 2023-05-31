705cfdd27ab9c93c45cfe926eaf21c7d658935b6
Edward <130746635+Plodder123@users.noreply.github.com>
2023-05-31 10:35:54 +0100

    seed changes and index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,25 @@ puts "cleaing database"
 
 puts "creating users"
 edward = User.create(email: "chicken@lewagon.com", password: "123456")
+User.create(email: 'user1@example.com', password: 'password1')
+User.create(email: 'user2@example.com', password: 'password2')
+User.create(email: 'user3@example.com', password: 'password3')
 
-Chicken.create(user_id, chicken_id)
 
-# Chicken.create!([{
-#   breed: "Wild chicken",
-#   description: "A wild angry chicken",
-#   price: "20"
-#   }])
+chicken_1 = Chicken.new(breed: "Wild chicken", description: "A wild angry chicken", price: "20")
+chicken_1.user = edward
+chicken_1.save!
+
+Chicken.create(breed: 'Rhode Island Red', description: 'Friendly and excellent layers.', price: 10.99, user_id: 1)
+Chicken.create(breed: 'Ameraucana', description: 'Known for their blue eggs.', price: 12.99, user_id: 1)
+Chicken.create(breed: 'Barred Plymouth Rock', description: 'Docile and good for families.', price: 9.99, user_id: 2)
+Chicken.create(breed: 'Silkie', description: 'Fluffy and adorable.', price: 11.99, user_id: 2)
+Chicken.create(breed: 'Buff Orpington', description: 'Gentle and good with children.', price: 13.99, user_id: 3)
+
+
+# Booking.create(user_id: 1, chicken_id: 1, start_date: Date.today, end_date: Date.today + 7)
+# Booking.create(user_id: 2, chicken_id: 2, start_date: Date.today + 3, end_date: Date.today + 10)
+# Booking.create(user_id: 3, chicken_id: 3, start_date: Date.today + 2, end_date: Date.today + 9)
+# Booking.create(user_id: 1, chicken_id: 4, start_date: Date.today + 5, end_date: Date.today + 12)
 
   p "There are #{Chicken.count} chickens"
