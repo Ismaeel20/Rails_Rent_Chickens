180a7440b4f1a9c9e5d7e4121c18b8f4ce8830b1
Warsame Jama <war.s@hotmail.co.uk>
2023-05-31 16:48:17 +0100

    cl img tag

--- app/controllers/chickens_controller.rb
+++ app/controllers/chickens_controller.rb
@@ -26,6 +26,6 @@ class ChickensController < ApplicationController
   private
 
   def chicken_params
-    params.require(:chicken).permit(:breed, :description, :price)
+    params.require(:chicken).permit(:breed, :description, :price, :photo)
   end
 end
